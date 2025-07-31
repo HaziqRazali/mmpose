88a442a5a02066486377cbfb76c9b802df375bec
HaziqRazali <ha_haziq_91@hotmail.com>
2025-07-31 11:07:28 +0800

    sync

--- run.sh
+++ run.sh
@@ -10,6 +10,13 @@ https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktai
 --input tests/data/coco/000000196141.jpg \
 --output-root vis_results/ --save-predictions
 
+python demo/topdown_demo_with_mmdet.py \
+demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
+https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
+configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
+https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
+--input "webcam" --show
+
 # rtmw-l_8xb320-270e_cocktail14-384x288
 python demo/topdown_demo_with_mmdet.py \
 demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
