4e631771481fb7f42f4ec359c7e62481cf259f16
HaziqRazali <ha_haziq_91@hotmail.com>
2025-12-08 18:13:29 +0800

    sync

--- run.sh
+++ run.sh
@@ -1,16 +1,3 @@
-
-python demo/offline_rom.py \
-demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input $HOME/datasets/telept/data/ipad/20251001-hh/rgb_1759733264296.mp4 \
-  --t1 00:00:10.833 \
-  --depth $HOME/datasets/telept/data/ipad/20251001-hh/depth_1759733264296.zip \
-  --show --show-3d --pcd-voxel 0.005
-
-#################### test
-
 # rtmw-l_8xb1024-270e_cocktail14-256x192
 python demo/topdown_demo_with_mmdet.py \
 demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
@@ -34,136 +21,8 @@ demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
 https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
 configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
 https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
---input "webcam" --show
-
-# realsense
-python demo/topdown_demo_with_mmdet.py \
-demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
---input "realsense" --show --only-rom-lines --show-depth --probe-depth
-
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --rgb-dir /home/haziq/datasets/telept/data/ipad/20251001-hh/frames_1759295456074/rgb/ \
-  --tasks /home/haziq/datasets/telept/data/ipad/20251001-hh/frames_1759295456074.json \
-  --no-show
-
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input /home/haziq/datasets/telept/data/ipad/20251001-hh/rgb_1759295456074.mp4 \
-  --rom-test left_elbow_flexion \
-  --t1 00:00:05.000 --t2 00:00:32.000 \
-  --rom-out angles_left_elbow.csv
-
-# home desktop + creative
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input webcam \
-  --show \
-  --voice --voice-mic "plughw:CARD=L1080p,DEV=0" \
-  --output-root out \
-  --auto-rom \
-  --rom-v-go 30 \
-  --rom-v-stop 10 \
-  --rom-hold-sec 0.6 \
-  --rom-std-max 1.5 \
-  --rom-min-amplitude 15 \
-  --rom-start-amp 8 \
-  --rom-baseline-tol 12 \
-  --rom-baseline-hold-sec 0.5 \
-  --rom-timeout-sec 25 \
-  --plot-seconds 0
-
-# home desktop + realsense
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input realsense \
-  --show \
-  --voice --voice-mic "plughw:CARD=L1080p,DEV=0" \
-  --output-root out \
-  --auto-rom \
-  --rom-v-go 30 \
-  --rom-v-stop 10 \
-  --rom-hold-sec 0.6 \
-  --rom-std-max 1.5 \
-  --rom-min-amplitude 15 \
-  --rom-start-amp 8 \
-  --rom-baseline-tol 12 \
-  --rom-baseline-hold-sec 0.5 \
-  --rom-timeout-sec 25 \
-  --plot-seconds 0
+--input "webcam" --device "cpu" --show
   
-# astar aftershock laptop + creative
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input "/dev/video2" \
-  --show \
-  --voice --voice-mic "plughw:CARD=L1080p,DEV=0" \
-  --output-root out \
-  --auto-rom \
-  --rom-v-go 30 \
-  --rom-v-stop 10 \
-  --rom-hold-sec 0.6 \
-  --rom-std-max 1.5 \
-  --rom-min-amplitude 15 \
-  --rom-start-amp 8 \
-  --rom-baseline-tol 12 \
-  --rom-baseline-hold-sec 0.5 \
-  --rom-timeout-sec 25 \
-  --plot-seconds 0
-
-# astar aftershock laptop + realsense
-python demo/topdown_demo_with_mmdet.py \
-  demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-  configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-  https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-  --input "realsense" \
-  --show \
-  --show-result \
-  --panel-window gallery \
-  --voice --voice-mic "default" \
-  --output-root out \
-  --auto-rom \
-  --rom-v-go 20 \
-  --rom-v-stop 10 \
-  --rom-hold-sec 0.6 \
-  --rom-std-max 4.0 \
-  --rom-min-amplitude 15 \
-  --rom-start-amp 8 \
-  --rom-baseline-tol 12 \
-  --rom-baseline-hold-sec 0.5 \
-  --rom-timeout-sec 25 \
-  --plot-seconds 0 \
-  --zero --abs-angle \
-  --debug
-
-# video
-python demo/topdown_demo_with_mmdet.py \
-demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
---input "/home/haziqmr/Downloads/Pathological_movements.mov" \
---output-root vis_results/ --save-predictions --debug
-
 # rtmw-l_8xb320-270e_cocktail14-384x288
 python demo/topdown_demo_with_mmdet.py \
 demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
@@ -173,18 +32,6 @@ https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktai
 --input tests/data/coco/000000196141.jpg \
 --output-root vis_results/ --save-predictions
 
-# look for mp4 files in /home/haziq/datasets/physio/ and uses them as input
-for f in /home/haziq/datasets/physio/*.mp4; do
-  python demo/topdown_demo_with_mmdet.py \
-    demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
-    https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
-    configs/wholebody_2d_keypoint/rtmpose/cocktail14/rtmw-l_8xb1024-270e_cocktail14-256x192.py \
-    https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122.pth \
-    --input "$f" \
-    --output-root vis_results/ \
-    --save-predictions
-done
-
 python demo/topdown_demo_with_mmdet.py \
 demo/mmdetection_cfg/rtmdet_m_640-8xb32_coco-person.py \
 https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth \
@@ -193,9 +40,6 @@ https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-dw-x-l_simcc-cocktai
 --input "/home/haziq/datasets/fit3d/data/fit3d/train/s05/videos/50591643/band_pull_apart.mp4" \
 --output-root "/home/haziq/datasets/fit3d/data/fit3d/train/s05/mmpose/rtmw-dw-x-l_simcc-cocktail14_270e-256x192-20231122/" \
 --save-predictions
-
-# /home/haziq/datasets/fit3d/data/train/*/videos/*/*.mp4
-# /media/haziq/Haziq/fit3d/data/train/*/videos/*/*.mp4
 
 # # # # # # # # # # #
 # for fit3d dataset #
